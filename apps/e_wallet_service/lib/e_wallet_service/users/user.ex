8c066bab3c3eccffd5faa6444e8f87b4c073ec61
tadeifelipe <felipe_santostadei@hotmail.com>
2024-02-03 14:07:51 -0300

    add users schema

---
+++ apps/e_wallet_service/lib/e_wallet_service/users/user.ex
@@ -0,0 +1,24 @@
+defmodule EWalletService.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @required_fields [:name, :email, :password, :password_hash]
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_fields)
+    |> validate_required(@required_fields)
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint([:email])
+  end
+end
